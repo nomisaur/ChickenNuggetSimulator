c9f056f4fce712cdbb54c48f52f0d864113b6f22
nomi <naomisaltis@gmail.com>
2025-09-14 09:09:51 -0700

    namespace

--- ChickenNuggetSimulator.Core/ChickenNuggetSimulatorGame.cs
+++ ChickenNuggetSimulator.Core/ChickenNuggetSimulatorGame.cs
@@ -7,108 +7,106 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace ChickenNuggetSimulator.Core
+namespace ChickenNuggetSimulator.Core;
+
+/// <summary>
+/// The main class for the game, responsible for managing game components, settings,
+/// and platform-specific configurations.
+/// </summary>
+public class ChickenNuggetSimulatorGame : Setup
 {
+    private Texture2D chicken;
+
     /// <summary>
-    /// The main class for the game, responsible for managing game components, settings,
-    /// and platform-specific configurations.
+    /// Indicates if the game is running on a mobile platform.
     /// </summary>
-    public class ChickenNuggetSimulatorGame : Setup
+    public readonly static bool IsMobile = OperatingSystem.IsAndroid() || OperatingSystem.IsIOS();
+
+    /// <summary>
+    /// Indicates if the game is running on a desktop platform.
+    /// </summary>
+    public readonly static bool IsDesktop =
+        OperatingSystem.IsMacOS() || OperatingSystem.IsLinux() || OperatingSystem.IsWindows();
+
+    /// <summary>
+    /// Initializes a new instance of the game. Configures platform-specific settings,
+    /// initializes services like settings and leaderboard managers, and sets up the
+    /// screen manager for screen transitions.
+    /// </summary>
+    public ChickenNuggetSimulatorGame()
+        : base("Chicken Nugget Simulator", 360, 640, false, DisplayOrientation.Portrait) { }
+
+    /// <summary>
+    /// Initializes the game, including setting up localization and adding the
+    /// initial screens to the ScreenManager.
+    /// </summary>
+    protected override void Initialize()
     {
-        private Texture2D chicken;
+        base.Initialize();
 
-        /// <summary>
-        /// Indicates if the game is running on a mobile platform.
-        /// </summary>
-        public readonly static bool IsMobile =
-            OperatingSystem.IsAndroid() || OperatingSystem.IsIOS();
-
-        /// <summary>
-        /// Indicates if the game is running on a desktop platform.
-        /// </summary>
-        public readonly static bool IsDesktop =
-            OperatingSystem.IsMacOS() || OperatingSystem.IsLinux() || OperatingSystem.IsWindows();
-
-        /// <summary>
-        /// Initializes a new instance of the game. Configures platform-specific settings,
-        /// initializes services like settings and leaderboard managers, and sets up the
-        /// screen manager for screen transitions.
-        /// </summary>
-        public ChickenNuggetSimulatorGame()
-            : base("Chicken Nugget Simulator", 360, 640, false, DisplayOrientation.Portrait) { }
-
-        /// <summary>
-        /// Initializes the game, including setting up localization and adding the
-        /// initial screens to the ScreenManager.
-        /// </summary>
-        protected override void Initialize()
+        // Load supported languages and set the default language.
+        List<CultureInfo> cultures = LocalizationManager.GetSupportedCultures();
+        var languages = new List<CultureInfo>();
+        for (int i = 0; i < cultures.Count; i++)
         {
-            base.Initialize();
-
-            // Load supported languages and set the default language.
-            List<CultureInfo> cultures = LocalizationManager.GetSupportedCultures();
-            var languages = new List<CultureInfo>();
-            for (int i = 0; i < cultures.Count; i++)
-            {
-                languages.Add(cultures[i]);
-            }
-
-            // TODO You should load this from a settings file or similar,
-            // based on what the user or operating system selected.
-            var selectedLanguage = LocalizationManager.DEFAULT_CULTURE_CODE;
-            LocalizationManager.SetCulture(selectedLanguage);
+            languages.Add(cultures[i]);
         }
 
-        /// <summary>
-        /// Loads game content, such as textures and particle systems.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            base.LoadContent();
-            chicken = Content.Load<Texture2D>("Assets/Chicken/chichen");
-        }
+        // TODO You should load this from a settings file or similar,
+        // based on what the user or operating system selected.
+        var selectedLanguage = LocalizationManager.DEFAULT_CULTURE_CODE;
+        LocalizationManager.SetCulture(selectedLanguage);
+    }
 
-        /// <summary>
-        /// Updates the game's logic, called once per frame.
-        /// </summary>
-        /// <param name="gameTime">
-        /// Provides a snapshot of timing values used for game updates.
-        /// </param>
-        protected override void Update(GameTime gameTime)
-        {
-            // Exit the game if the Back button (GamePad) or Escape key (Keyboard) is pressed.
-            if (
-                GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
-                || Keyboard.GetState().IsKeyDown(Keys.Escape)
-            )
-                Exit();
+    /// <summary>
+    /// Loads game content, such as textures and particle systems.
+    /// </summary>
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+        chicken = Content.Load<Texture2D>("Assets/Chicken/chichen");
+    }
 
-            // TODO: Add your update logic here
+    /// <summary>
+    /// Updates the game's logic, called once per frame.
+    /// </summary>
+    /// <param name="gameTime">
+    /// Provides a snapshot of timing values used for game updates.
+    /// </param>
+    protected override void Update(GameTime gameTime)
+    {
+        // Exit the game if the Back button (GamePad) or Escape key (Keyboard) is pressed.
+        if (
+            GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed
+            || Keyboard.GetState().IsKeyDown(Keys.Escape)
+        )
+            Exit();
 
-            base.Update(gameTime);
-        }
+        // TODO: Add your update logic here
 
-        /// <summary>
-        /// Draws the game's graphics, called once per frame.
-        /// </summary>
-        /// <param name="gameTime">
-        /// Provides a snapshot of timing values used for rendering.
-        /// </param>
-        protected override void Draw(GameTime gameTime)
-        {
-            // Clears the screen with the MonoGame orange color before drawing.
-            GraphicsDevice.Clear(Color.Green);
+        base.Update(gameTime);
+    }
 
-            // Begin the sprite batch to prepare for rendering.
-            SpriteBatch.Begin();
+    /// <summary>
+    /// Draws the game's graphics, called once per frame.
+    /// </summary>
+    /// <param name="gameTime">
+    /// Provides a snapshot of timing values used for rendering.
+    /// </param>
+    protected override void Draw(GameTime gameTime)
+    {
+        // Clears the screen with the MonoGame orange color before drawing.
+        GraphicsDevice.Clear(Color.Green);
 
-            // Draw the chicken texture
-            SpriteBatch.Draw(chicken, Vector2.Zero, Color.White);
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin();
 
-            // Always end the sprite batch when finished.
-            SpriteBatch.End();
+        // Draw the chicken texture
+        SpriteBatch.Draw(chicken, Vector2.Zero, Color.White);
 
-            base.Draw(gameTime);
-        }
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
+
+        base.Draw(gameTime);
     }
 }
